598085ab24fa4e491699051fa7baf00e752c0f9a
wangyl19 <wangyl19@asiainfo-sec.com>
2018-10-07 09:29:19 +0800

    init pms

--- src/main/java/com/asiainfo/demo/DemoApplication.java
+++ src/main/java/com/asiainfo/demo/DemoApplication.java
@@ -3,12 +3,20 @@ package com.asiainfo.demo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @MapperScan("com.asiainfo.demo.dao")
-public class DemoApplication {
- 
+public class DemoApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DemoApplication.class);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
+
 }
